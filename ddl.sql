6c1dbcb5e02212e2fba192fefb12068e16fbf28c
Anartz <anartz.pagaldia@gmail.com>
2023-10-11 19:18:11 +0200

    ddl cambiado

--- ddl.sql
+++ ddl.sql
@@ -4,25 +4,27 @@ drop table preguntas;
 drop table respuestas;
 
 create table colecciones (
-    id int(1), 
+    id int(1) primary key, 
     nombre varchar(20)
 );
 
 create table tests (
-    id int(2), 
-    idColeciones int(1) references colecciones(id)
+    id int(2) primary key, 
+    idColecciones int(1) references colecciones(id)
 );
 
 create table preguntas (
-    id int(2), 
+    id int(2) primary key, 
     idTests int(1) references tests(id),
-    pregunta varchar(100)
+    pregunta varchar(100),
+    ayuda varchar(1000)
 );
 
 create table respuestas (
-    id int(1),
+    id int(1) auto_increment primary key,
     idPreuntas int(1) references preguntas(id),
-    respuesta varchar(100)
+    respuesta varchar(100),
+    correcta int(1)
 );
 
 
